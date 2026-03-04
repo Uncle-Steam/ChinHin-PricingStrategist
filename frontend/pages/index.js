e4f1b91cece9fff6071f26bb021d94260f58415e
Janson <hohohojanson@gmail.com>
2026-03-04 15:28:36 +0800

    initialize repo

---
+++ frontend/pages/index.js
@@ -0,0 +1,15 @@
+import SmallCard from '../components/SmallCard';
+import { projectIcons } from '../components/Icons';
+
+import { projects } from '../utils/projectsData';
+
+const Home = () => (
+  <div className="home">
+    <h1>What Can I Deploy to Static Apps?</h1>
+    <div className="card-grid">
+      <p>hello world test2</p>
+    </div>
+  </div>
+);
+
+export default Home;
